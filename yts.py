29c9c44d89e354299474da9e708236c993a1edf9
Khen Solomon Lethil <khensolomon@gmail.com>
2018-11-06 16:57:00 +0100

    fixed for python 2.7

--- yts.py
+++ yts.py
@@ -1,9 +1,6 @@
-# VERSION: 3.3
+# VERSION: 3.4
 # AUTHORS: Khen Solomon Lethil (khensolomon@gmail.com)
-import json
-import time
-import re
-import math
+import json, time, re, math
 try:
     # python3
     from urllib.parse import urlencode, unquote, quote_plus
@@ -84,30 +81,31 @@ class yts(object):
                 params['query_term'] = query_term_param
                 # params['query_term'] = quote_plus(query_term_param)
 
-        url = scriptive.urlBuilder(self.url,['api', 'v2', 'list_movies.json'],params)
+        job = scriptive()
+        url = job.urlBuilder(self.url,['api', 'v2', 'list_movies.json'],params)
         data = retrieve_url(url)
         j = json.loads(data)
         if j['data']['movie_count']:
             result_page = str(j['data']['page_number'])+'of'+str(math.ceil(j['data']['movie_count'] / j['data']['limit']))
             for movies in j['data']['movies']:
                 for torrent in movies['torrents']:
-                    res = {'link':scriptive.magnetBuilder(torrent['hash'],movies['title']),
+                    res = {'link':job.magnetBuilder(torrent['hash'],movies['title']),
                            'name': '{n} ({y}) [{q}]-[{p}]-[{i}]'.format(n=movies['title'], y=movies['year'], q=torrent['quality'], p=result_page, i=self.name),
                            'size': torrent['size'],
                            'seeds': torrent['seeds'],
                            'leech': torrent['peers'],
                            'engine_url': 'IMDB:{rating}, [{genres}]'.format(rating=movies['rating'], genres=', '.join(movies['genres'])),
                            'desc_link': movies['url']}
-                    scriptive.urlResponse(res)
-        elif scriptive.supported_browse_movies:
+                    job.urlResponse(res)
+        elif job.supported_browse_movies:
             urlPath={'query_term':'0', 'quality':'all','genre':'all','minimum_rating':'0','sort_by':'latest'}
             for i in urlPath:
                 if i in params:
                     urlPath[i]=params[i]
             urlPath = list(urlPath.values())
-            urlPath.insert(0,scriptive.supported_browse_movies)
+            urlPath.insert(0,job.supported_browse_movies)
 
-            url = scriptive.urlBuilder(self.url,urlPath,'page' in params and {'page':params['page']})
+            url = job.urlBuilder(self.url,urlPath,'page' in params and {'page':params['page']})
             data = retrieve_url(url)
             data = re.sub("\s\s+", "", data).replace('\n', '').replace('\r', '')
 
@@ -122,19 +120,19 @@ class yts(object):
                 response_detail = re.sub("\s\s+", "", response_detail).replace('\n', '').replace('\r', '')
                 movie_id = re.findall('data-movie-id="(\d+)"', response_detail)[0]
                 if movie_id:
-                    url = scriptive.urlBuilder(self.url,['api', 'v2', 'movie_details.json'],{'movie_id':movie_id})
+                    url = job.urlBuilder(self.url,['api', 'v2', 'movie_details.json'],{'movie_id':movie_id})
                     data_detail = retrieve_url(url)
                     j = json.loads(data_detail)
                     movies = j['data']['movie']
                     for torrent in movies['torrents']:
-                        res = {'link':scriptive.magnetBuilder(torrent['hash'],movies['title']),
+                        res = {'link':job.magnetBuilder(torrent['hash'],movies['title']),
                                'name': '{n} ({y}) [{q}]-[{p}]-[{i}]'.format(n=movies['title'], y=movies['year'], q=torrent['quality'], p=result_page,i=self.name[:-1]),
                                'size': torrent['size'],
                                'seeds': torrent['seeds'],
                                'leech': torrent['peers'],
                                'engine_url': 'IMDB:{rating}, [{genres}]'.format(rating=movies['rating'], genres=', '.join(movies['genres'])),
                                'desc_link': movies['url']}
-                        scriptive.urlResponse(res)
+                        job.urlResponse(res)
                 else:
                     movie_title = re.findall('<a.*?class="browse-movie-title".*?>(.*?)</a>', hM)[0]
                     movie_year = re.findall('<div.?class="browse-movie-year".*?>(.*?)</div>', hM)[0]
@@ -145,32 +143,32 @@ class yts(object):
                     movie_genre = re.findall('<figcaption class=".*?">.*?(<h4>.*</h4>).*?</figcaption>', hM)[0]
                     movie_genre = re.findall('<h4>(.*?)</h4>', movie_genre)
                     # print(movie_title,movie_link,movie_year,movie_rate,movie_genre)
-                    scriptive.urlResponse()
+                    job.urlResponse()
         else:
-            scriptive.urlResponse()
+            job.urlResponse()
 
 class scriptive(object):
     supported_browse_movies = 'browse-movies'
+    tracker = ['udp://open.demonii.com:1337/announce',
+            'udp://tracker.openbittorrent.com:80',
+            'udp://tracker.coppersurfer.tk:6969',
+            'udp://glotorrents.pw:6969/announce',
+            'udp://tracker.opentrackr.org:1337/announce',
+            'udp://torrent.gresille.org:80/announce',
+            'udp://p4p.arenabg.com:1337',
+            'udp://tracker.leechers-paradise.org:6969']
 
-    def magnetBuilder(hash, name):
-        tr_tracker = ['udp://open.demonii.com:1337/announce',
-                    'udp://tracker.openbittorrent.com:80',
-                    'udp://tracker.coppersurfer.tk:6969',
-                    'udp://glotorrents.pw:6969/announce',
-                    'udp://tracker.opentrackr.org:1337/announce',
-                    'udp://torrent.gresille.org:80/announce',
-                    'udp://p4p.arenabg.com:1337',
-                    'udp://tracker.leechers-paradise.org:6969']
-        return "magnet:?xt=urn:btih:{}&{}&{}".format(hash,urlencode({'dn': name}),'&'.join(map(lambda x: 'tr='+quote_plus(x.strip()), tr_tracker)))
+    def magnetBuilder(self,hash, name):
+        return "magnet:?xt=urn:btih:{}&{}&{}".format(hash,urlencode({'dn': name}),'&'.join(map(lambda x: 'tr='+quote_plus(x.strip()), self.tracker)))
 
-    def urlBuilder(url, uri=[], param={}):
+    def urlBuilder(self, url, uri=[], param={}):
         for r in uri:
             url = '{}/{}'.format(url, r)
         if param:
             url = '{}?{}'.format(url, urlencode(param))
         return url
 
-    def urlResponse(res={}):
+    def urlResponse(self,res={}):
         if res:
             # print(res['name'])
             prettyPrinter(res)
